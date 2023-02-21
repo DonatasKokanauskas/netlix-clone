89c5a35cfc5693d36b1a71c656934f952dde9bbb
donatas.kokanauskas <donatas.kokanauskas@gmail.com>
2023-02-21 16:47:47 +0200

    movie-info

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import MainImage from "./components/MainImage";
+import MovieInfo from "./components/MovieInfo";
 
 function App() {
   return (
     <div>
       <Header />
       <MainImage />
+      <MovieInfo />
     </div>
   );
 }
---
+++ src/components/MovieInfo.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "../style/css/movieInfo.css";
+import TitleLogo from "../images/titleLogo.webp";
+import { AiFillCaretRight } from "react-icons/ai";
+import { AiOutlineInfoCircle } from "react-icons/ai";
+
+const MovieInfo = () => {
+  return (
+    <div className="movie-info">
+      <img src={TitleLogo} alt="title logo" />
+      <p>
+        Feeling spurned after being cut from the national team, newly single
+        softball player Lisa finds herself in a heated love triangle.
+      </p>
+      <div>
+        <button>
+          <AiFillCaretRight />
+          Play
+        </button>
+        <button>
+          <AiOutlineInfoCircle />
+          More info
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default MovieInfo;
