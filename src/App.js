ff1da92544c20250f5ee9f0e2d0a90af8d6efab8
donatas.kokanauskas <donatas.kokanauskas@gmail.com>
2023-02-22 18:05:53 +0200

    fetch-data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import "./style/css/app.css";
 import Header from "./components/Header";
 import MainImage from "./components/MainImage";
 import MovieInfo from "./components/MovieInfo";
+import MoviesCards from "./components/MoviesCards";
 
 function App() {
   return (
-    <div>
+    <div className="app-container">
       <Header />
       <MainImage />
       <MovieInfo />
+      <MoviesCards />
     </div>
   );
 }
